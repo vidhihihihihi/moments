eee18851f801ccb2b1229251a8518e321a4587eb
vidhihihihihi <gupta.74@iitj.ac.in>
2023-08-09 15:29:26 +0530

    first commit

---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Post from './Post/Post';
+import useStyles from './styles';
+import { useSelector } from 'react-redux';
+import { Grid, CircularProgress } from '@material-ui/core';
+
+const Posts = () => {
+    const posts = useSelector((state)=> state.posts);
+    const classes= useStyles();
+    console.log(posts);
+    return (
+        !posts.length ? <CircularProgress/> : (
+          <Grid>
+            
+          </Grid>
+        )
+    );
+}
+
+export default Posts;
---
+++ server/routes/posts.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import { getPosts,createPost } from '../controllers/posts.js';
+const router = express.Router();
+
+//http://localhost:5000/posts
+router.get('/',getPosts);
+router.get('/',createPost);
+
+export default router;
