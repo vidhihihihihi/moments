ab34fa2a14f54f09310849d7c0d0849a7525837d
vidhihihihihi <gupta.74@iitj.ac.in>
2023-09-20 16:19:28 +0530

    comment

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 
 //https://www.mongodb.com/cloud/atlas
-
+ 
 //const CONNECTION_URL = 'mongodb+srv://vidhigupta:mongodb@cluster0.jcuofxx.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
@@ -30,4 +30,4 @@ mongoose.Promise = global.Promise;
 */
 //mongoose.set('useFindAndModify',false);
 
-
+//you are a good girl
