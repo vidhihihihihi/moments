13ad9307296579266aaceebcb461297ae265e4f4
vidhihihihihi <gupta.74@iitj.ac.in>
2023-09-20 16:07:03 +0530

    config commit

--- server/index.js
+++ server/index.js
@@ -2,11 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
 
 
 const app = express();
-
+dotenv.config();
 
 
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
@@ -17,7 +18,7 @@ app.use('/posts', postRoutes);
 
 //https://www.mongodb.com/cloud/atlas
 
-const CONNECTION_URL = 'mongodb+srv://vidhigupta:mongodb@cluster0.jcuofxx.mongodb.net/?retryWrites=true&w=majority';
+//const CONNECTION_URL = 'mongodb+srv://vidhigupta:mongodb@cluster0.jcuofxx.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL) .then(() => app.listen(PORT, () => console.log(`Server running on Port: http://localhost:${PORT}`)) ).catch((error) => console.log(`${error} did not connect`));
