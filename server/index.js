5308d419bd35bcddd5024323761fb62369a11720
vidhihihihihi <gupta.74@iitj.ac.in>
2023-09-21 12:56:25 +0530

    final

--- server/index.js
+++ server/index.js
@@ -18,14 +18,8 @@ app.use('/posts', postRoutes);
 
 //https://www.mongodb.com/cloud/atlas
  
-//const CONNECTION_URL = 'mongodb+srv://vidhigupta:mongodb@cluster0.jcuofxx.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL) .then(() => app.listen(PORT, () => console.log(`Server running on Port: http://localhost:${PORT}`)) ).catch((error) => console.log(`${error} did not connect`));
 
 mongoose.Promise = global.Promise;
-/*mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
-   .then(() => app.listen(PORT, ()=> console.log(`Server running on port: ${PORT}`)))
-   .catch((error)=> console.log(error.message));
-*/
-//mongoose.set('useFindAndModify',false);
